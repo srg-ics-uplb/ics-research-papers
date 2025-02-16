6a7eaac80776d435008690c48f77cd3c779e0189
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2025-02-16 10:50:11 +0800

    edits

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-rm ics-publications.bib
-cat bib/*.bib > index.bib
-bibtex2html --revkeys -d -r -t "UPLB Institute of Computer Science Reseach Publications" -o index_all -css style.css index.bib 
 rm *.aux *.bbl *.blg bib2html*
+cat bib/*.bib > ics-publications.bib
+pdflatex ics-publications
+bibtex ics-publications
+bibtex ics-publications
+bibtex ics-publications
+pdflatex ics-publications
+#bibtex2html --revkeys -d -r -t "UPLB Institute of Computer Science Reseach Publications" -o index_all -css style.css index.bib 
+#rm *.aux *.bbl *.blg bib2html*
---
+++ clean.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm *.aux *.bbl *.bcf *.blg *.log *.out *.run.xml
